68198b678029e0457cecfbe46169bce675d4df03
猫猫球 <45891684+FanSun01@users.noreply.github.com>
2022-08-06 23:48:33 +0800

    Changes

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("men ge tai zha le");
+        var arr = new SimpleTools();
+        arr.bubble(new int[]{1, 4, 6, 7, 6, 8, 2});
+    }
+
+}
---
+++ src/SimpleTools.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class SimpleTools {
+    public void bubble(int[] array) {
+        int temp = 0;
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        for (int k = 0; k < array.length; k++) {
+            System.out.println(array[k]);
+        }
+
+    }
+}
